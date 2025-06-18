a3581276a8e6cbf3b8a3bbc3ef8ea5272aa4f064
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2025-06-18 22:36:36 +0900

    Added param_utils.py.

---
+++ src/utils/param_utils.py
@@ -0,0 +1,24 @@
+# utils/param_utils.py
+
+from utils import config
+
+# --- window list -----------------------------------------------------
+def windows() -> list[int]:
+    return config.getlist("pipeline", "windows", int)
+
+# --- N helpers -------------------------------------------------------
+_USE_SCALE = config.get('knn', 'use_week_scaling', bool)
+_TRAIN_WEEKS = config.get('knn', 'train_weeks', int)
+
+def _N_cfg_list() -> list[int]:
+    return config.getlist("knn", "Ns_week", int)
+
+def _N_effective(N_cfg: int) -> int:
+    return N_cfg * _TRAIN_WEEKS if _USE_SCALE else N_cfg
+
+def N_all_effective() -> list[int]:
+    return [_N_effective(n) for n in _N_cfg_list()]
+
+# --- theta list ------------------------------------------------------
+def thetas() -> list[int]:
+    return config.getlist("knn", "thetas", int)
